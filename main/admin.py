817bde48d6635bbfc0c3ddd04261bbc760ca77a6
syedbilal28 <syed.bilal.sba@gmail.com>
2022-06-08 14:57:49 +0500

    packages fix

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import CostModel, Request,Profile,Feedback
+from .models import CostModel, Request,Profile,Feedback,Attachment
 # Register your models here.
 admin.site.register(Request)
 admin.site.register(Profile)
 admin.site.register(Feedback)
 admin.site.register(CostModel)
+admin.site.register(Attachment)
